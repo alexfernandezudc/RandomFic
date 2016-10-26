623f2c297064c228026a3962b151b6b7c742891f
brais <brais.mcastro@udc.es>
2016-10-26 22:34:54 +0200

    para el sprint 2

---
+++ app/src/main/java/com/example/brais/myapplication/Category.java
@@ -0,0 +1,14 @@
+package com.example.brais.myapplication;
+
+import java.util.ArrayList;
+
+/**
+ * Created by brais on 26/10/16.
+ */
+
+public class Category {
+
+    private ArrayList<String> items = new ArrayList<>();
+
+
+}
