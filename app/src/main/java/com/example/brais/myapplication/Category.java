fe4eb05c564507541166205096a2398065a397c4
brais <brais.mcastro@udc.es>
2016-10-27 13:11:27 +0200

    métodos de category.java implementados. + Ahora extiende de ArrayList<String>

--- app/src/main/java/com/example/brais/myapplication/Category.java
+++ app/src/main/java/com/example/brais/myapplication/Category.java
@@ -6,11 +6,18 @@ import java.util.ArrayList;
  * Created by brais on 26/10/16.
  */
 
-public class Category extends ArrayList{
+public class Category extends ArrayList<String>{
 
     private String name = null;
 
     public Category(String name){
         this.name = name;
     }
+
+    public String getName(){return this.name;}
+
+    public void setName(String newName){this.name = newName;}
+
+    @Override
+    public String toString(){return this.name;}
 }
