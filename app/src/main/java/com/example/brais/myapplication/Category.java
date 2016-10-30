dde3ed92e8e43c6da9220dff2116a71e683a0022
brais <brais.mcastro@udc.es>
2016-10-30 22:01:01 +0100

    nueva implementación de category

--- app/src/main/java/com/example/brais/myapplication/Category.java
+++ app/src/main/java/com/example/brais/myapplication/Category.java
@@ -6,17 +6,25 @@ import java.util.ArrayList;
  * Created by brais on 26/10/16.
  */
 
-public class Category extends ArrayList<String>{
+public class Category{
 
     private String name = null;
+    private ArrayList<String> items;
 
     public Category(String name){
         this.name = name;
+        this.items = new ArrayList<>();
     }
 
     public String getName(){return this.name;}
 
     public void setName(String newName){this.name = newName;}
+
+    public ArrayList<String> getItems() { return this.items;}
+
+    public void newItem(String newItem){
+        items.add(newItem);
+    }
 
     @Override
     public String toString(){return this.name;}
