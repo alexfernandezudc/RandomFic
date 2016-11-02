6e9d2f93bfa834d57e857dcc5ceddcb431f97452
Brais <brais.mcastro@udc.es>
2016-11-02 20:43:45 +0100

    se puede editar el texto visible de un item dentro de una categoría pero la modificacion no es persistente

--- app/src/main/java/view/CategoryActivity.java
+++ app/src/main/java/view/CategoryActivity.java
@@ -1,11 +1,17 @@
 package view;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import com.example.brais.myapplication.R;
@@ -36,13 +42,44 @@ public class CategoryActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        // Nombre de la actividad
+        setTitle(category.getName());
+
         // Preparamos el adaptador
         final OurAdapter adapter = new OurAdapter(category.getItems(), this);
         lista = (ListView) findViewById(R.id.listView);
         lista.setAdapter(adapter);
         lista.setLongClickable(true);
 
-
+        lista.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> arg0, View view, final int pos, long id) {
+                // Instanciamos el dialogo y añadimos el manejador del botón aceptar
+                AlertDialog.Builder builder = new AlertDialog.Builder(CategoryActivity.this);
+                LayoutInflater inflater = getLayoutInflater();
+                final View v = inflater.inflate(R.layout.input_dialog, null);
+                builder.setView(v);
+                builder.setTitle(R.string.dialog_title_edit_category);
+                // Handler botón aceptar del diálogo.
+                builder.setPositiveButton(R.string.dialog_positive_button_label, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface d, int which) {
+                        EditText editText = (EditText) v.findViewById(R.id.categoryInput);
+                        category.getItems().set(pos,editText.getText().toString());
+                        lista.invalidateViews();
+                    }
+                });
+                // Handler botón cancelar del diálogo
+                builder.setNegativeButton(R.string.dialog_negative_button_label, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface d, int which) {
+                        d.cancel();
+                    }
+                });
+                builder.show();
+                return true;
+            }
+        });
     }
 
 
