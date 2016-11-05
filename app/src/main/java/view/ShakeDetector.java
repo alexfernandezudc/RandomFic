4fceef945dc18d11307b2fa1804cfbb4f27b1526
Brais <brais.mcastro@udc.es>
2016-11-05 17:55:57 +0100

    Ahora en Category Actvity se selecciona un item aleatorio dejando el dispositivo "boca abajo" más de 1,5 seg

--- app/src/main/java/view/ShakeDetector.java
+++ app/src/main/java/view/ShakeDetector.java
@@ -18,10 +18,12 @@ public class ShakeDetector implements SensorEventListener {
     private static final float SHAKE_THRESHOLD_GRAVITY = 2.7F;
     private static final int SHAKE_SLOP_TIME_MS = 500;
     private static final int SHAKE_COUNT_RESET_TIME_MS = 3000;
+    private static final int UPSIDE_DOWN_REQUIRED_TIME = 1500;
 
     private OnShakeListener mListener;
     private long mShakeTimestamp;
     private int mShakeCount;
+    private boolean isUpsideDown = false;
 
     public void setOnShakeListener(OnShakeListener listener) {
         this.mListener = listener;
@@ -51,6 +53,19 @@ public class ShakeDetector implements SensorEventListener {
             // gForce will be close to 1 when there is no movement.
             float gForce = (float)Math.sqrt(gX * gX + gY * gY + gZ * gZ);
 
+            final long now = System.currentTimeMillis();
+            if (z > -5){ // Si el smartphone está boca abajo
+                if (isUpsideDown == false)  // Y acaba de cambiarse
+                    mShakeTimestamp = now;  // Tomamos nota del momento
+                isUpsideDown = true;
+            } else {    // Si está boca arriba
+                if (now - mShakeTimestamp > UPSIDE_DOWN_REQUIRED_TIME) {  // Comprobamos que haya pasado el tiempo necesario
+                    mListener.onShake(0);
+                    mShakeTimestamp = now;
+                }
+                isUpsideDown = false;   // Anotamos que ahora ya no está boca abajo
+            }
+/**
             if (gForce > SHAKE_THRESHOLD_GRAVITY) {
                 final long now = System.currentTimeMillis();
                 // ignore shake events too close to each other (500ms)
@@ -68,6 +83,7 @@ public class ShakeDetector implements SensorEventListener {
 
                 mListener.onShake(mShakeCount);
             }
+ **/
         }
     }
 }
